063a0051f230a15ca96d29062e4a31eb838e7078
Nick Benedetto <nickbenedetto@nicks-mbp.fios-router.home>
2017-05-08 23:32:26 -0400

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nick Benedetto on 5/8/17.
+//  Copyright © 2017 Nick Benedetto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
